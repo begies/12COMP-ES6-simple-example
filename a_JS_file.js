9400d72e6f4ae4794c92a07ddd4e39be2464261a
Bryan Gillies <bryan.gillies@hvhs.school.nz>
2025-03-20 16:51:12 +1300

    finish?

--- a_JS_file.js
+++ a_JS_file.js
@@ -15,15 +15,6 @@ function a_func_1() {
         'color: ' + A_COL_C + '; background-color: ' + A_COL_B + ';');
   b_func_1();
 }
-
-/**************************************************************/
-// a_func_2()
-// Called by b_func_2() in b_JS_file.js
-/**************************************************************/
-function a_func_2() {
-  console.log('%c a_func_2(): Called by b_func_2() in b_JS_file.js',
-              'color: ' + A_COL_C + '; background-color: ' + A_COL_B + ';');
-}
          
 /**************************************************************/
 //   END OF APP
--- b_JS_file.js
+++ b_JS_file.js
@@ -14,16 +14,6 @@ function b_func_1() {
 console.log('%c b_func_1(): Called by a_func_1() in a_JS_file.js',
             'color: ' + B_COL_C + '; background-color: ' + B_COL_B + ';');
 }
-
-/**************************************************************/
-// b_func_2()
-// Called by body onload event in index.html
-/**************************************************************/
-function b_func_2() {
-console.log('%c b_func_2(): Called by body onload event in index.html',
-            'color: ' + B_COL_C + '; background-color: ' + B_COL_B + ';');
-a_func_2();
-}
  
 /**************************************************************/
 //   END OF APP
